c8421edab0e2d35956878c1d46304d6cae21cc66
krynju <gulinski.krystian@gmail.com>
2021-12-10 00:23:12 +0100

    dtable fixes

--- dask/rundocker.sh
+++ dask/rundocker.sh
@@ -1,3 +1,2 @@
 docker build -t dask .
-PP=$PATH
 docker run -v C:\Users\krynjupc\WS\mgr_benchmark_setup:/home dask
---
+++ dtable/rundocker.sh
@@ -0,0 +1,2 @@
+docker build -t dtable .
+docker run -v C:\Users\krynjupc\WS\mgr_benchmark_setup:/home dtable
