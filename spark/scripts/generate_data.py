d1bdaebc920be435d2d7981e5986c0de0bb6cab7
krynju <gulinski.krystian@gmail.com>
2021-12-13 00:18:34 +0100

    update data gen and benchmarking

--- spark/scripts/generate_data.py
+++ spark/scripts/generate_data.py
@@ -10,13 +10,19 @@ unique_values = int(sys.argv[2])
 ncolumns = int(sys.argv[3])
 max_chunksize = 10000000
 
+import concurrent.futures
 if __name__ == '__main__':
 
     tablesize = 4 * ncolumns * n / 1_000_000
     print("@@@ TABLESIZE:       {} MB".format(tablesize))
-    df = pd.DataFrame(np.random.randint(0, unique_values-1, size=(n, ncolumns)),columns=['a1', 'a2', 'a3', 'a4'])
     nchunks = int((n+max_chunksize-1)/max_chunksize)
     os.mkdir("data")
 
-    for idx, chunk in enumerate(np.array_split(df, nchunks)):
-        chunk.to_csv(f'data/datapart_{idx}.csv', index=False)
+    def gen_and_save(idx):
+        df = pd.DataFrame(np.random.randint(0, unique_values-1, size=(max_chunksize, ncolumns)),columns=['a1', 'a2', 'a3', 'a4'])
+        df.to_csv(f'data/datapart_{idx}.csv', index=False)
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+        for idx in range(nchunks):
+            executor.submit(gen_and_save, idx)
+
